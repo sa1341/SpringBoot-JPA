daf38e539e2680f58cedbe9f938b4bbaea492cb1
임준영 <a79007714@gmail.com>
2019-10-04 23:49:12 +0900

    상품등록

--- src/main/java/jpabook/jpashop/controller/HomeController.java
+++ src/main/java/jpabook/jpashop/controller/HomeController.java
@@ -1,8 +1,13 @@
 package jpabook.jpashop.controller;
 
+import jpabook.jpashop.domain.Member;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @Slf4j
@@ -13,5 +18,8 @@ public class HomeController {
         log.info("home controller");
         return "home";
     }
+
+
+
 
 }
--- src/main/java/jpabook/jpashop/controller/MemberController.java
+++ src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -19,16 +20,16 @@ public class MemberController {
     private final MemberService memberService;
 
     @GetMapping("/members/new")
-    public String createForm(Model model){
+    public String createForm(Model model) {
         model.addAttribute("memberForm", new MemberForm());
         return "members/createMemberForm";
     }
 
     @PostMapping("/members/new")
-    public String create(@Valid MemberForm form, BindingResult result){ // 바인딩 객체를 쓰지 않으면 검증실패시 컨트롤러 단에서 튕겨저 나가서 /error로 빠진다.
+    public String create(@Valid MemberForm form, BindingResult result) { // 바인딩 객체를 쓰지 않으면 검증실패시 컨트롤러 단에서 튕겨저 나가서 /error로 빠진다.
         // BindingResult는 MemberForm에서 스프링에서 검증을 하는데 실패가 나면 에러내용를 담고 있는 객체로 다시 뷰단으로 끌고 가는 기능을 가지고 있음.
         //Client 에서 요청한 MemberForm에 바인딩 된 값도 그대로 다시 가져오기 때문에 회원가입 폼에도 다시 고객이 작성한 내용이 그대로 보존된다.
-        if(result.hasErrors()){
+        if (result.hasErrors()) {
             return "members/createMemberForm";
         }
 
@@ -42,5 +43,15 @@ public class MemberController {
 
         return "redirect:/";
     }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        //cmd + alt + n 인라인기능... 변수를 합친다.
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+
+        return "members/memberList";
+    }
+
 
 }
