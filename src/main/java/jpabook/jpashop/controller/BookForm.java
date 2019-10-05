cf6c32c3b19602a52b58a9571dccd6208cd81c3e
임준영 <a79007714@gmail.com>
2019-10-05 18:16:08 +0900

    변경감지 메소드 추가

--- src/main/java/jpabook/jpashop/controller/BookForm.java
+++ src/main/java/jpabook/jpashop/controller/BookForm.java
@@ -11,8 +11,7 @@ public class BookForm {
     private Long id;
     private String name;
     private int price;
-    private  int stockQuantity;
-
+    private int stockQuantity;
     private String author;
     private String isbn;
 
